ea5d839a812432a263b42b4196db3e2247616f5d
Jeferson Silva <jeferson.s.silva1@gmail.com>
2019-10-31 20:35:44 -0300

    exercicio 1

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    name: 'Quentin Tarantino',
+    occupation: 'Director',
+    catchPharse: 'Some Phrarse'
+  },
+  
+  {
+    name: 'Quentin Tarantino',
+    occupation: 'Director',
+    catchPharse: 'Some Phrarse'
+  },
+  
+  {
+    name: 'Quentin Tarantino',
+    occupation: 'Director',
+    catchPharse: 'Some Phrarse'
+  }
+];
