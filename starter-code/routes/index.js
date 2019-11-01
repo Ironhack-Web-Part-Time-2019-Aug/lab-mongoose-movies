6a3ad523c5f628335835fbfdd9b590b7e08d8339
Jeferson Silva <jeferson.s.silva1@gmail.com>
2019-10-31 22:22:55 -0300

    criado rota celebrities

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router  = express.Router();
+const Celebrity = require('../models/Celebrity');
 
 /* GET home page */
 router.get('/', (req, res) => {
   res.render('index');
+});
+
+router.get('/celebrities', (req, res, next) => {
+  Celebrity
+    .find({}) // retorna todos documentos
+    .then( // sucesso
+      (celebrities) => {
+        res.send(celebrities);
+      }
+    )
+    .catch( // se der erro
+      (error) => {
+        console.log(error);
+        res.send(error);
+        next();
+      }
+    );
 });
 
 module.exports = router;
